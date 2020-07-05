7e65b9cc0fb019b52253bd7455969efdfa56781c
nishchay2698 <nischay.garala@gmail.com>
2020-07-05 14:55:14 +0000

    create .py

---
+++ stpl_1/config/stpl_1.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+        return [
+        {
+                "label": _("stpl_1"),
+                "icon": "octicon octicon-briefcase",
+                "items": [
+                {
+                        "type": "doctype",
+                        "name": "First Doctype",
+                        "label": _("First Doctype"),
+                },
+                
+                ]
+        }
+]
+
