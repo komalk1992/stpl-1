fa75ce84d418448a6e6075d2e87ebbc51eb3e276
nishchay2698 <nischay.garala@gmail.com>
2020-07-07 15:05:15 +0000

    other so and delviery doctype created

---
+++ stpl_1/stpl_1/doctype/stockist_sales_order/stockist_sales_order.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, umesh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stockist Sales Order', {
+	// refresh: function(frm) {
+
+	// }
+});
